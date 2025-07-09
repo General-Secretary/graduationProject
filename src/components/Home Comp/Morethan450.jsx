fc3ddf32ccbbfcaae9b44c02631cda045606d35d
General-Secretary <omar1313zedan@gmail.com>
2025-07-09 03:59:16 +0300

    تحديث المشروع بعد التعديلات

--- src/components/Home Comp/Morethan450.jsx
+++ src/components/Home Comp/Morethan450.jsx
@@ -17,7 +17,26 @@ export default function MoreThan450() {
                 <div className='md:w-1/3 w-full'>
                     <div className='sm:w-[70%]  my-5 md:w-[80%] lg:w-[70%]  mx-auto overflow-hidden'>
                         <div className='relative rounded-2xl overflow-hidden bg-slate-400'>
-                            <Image unoptimized src={"/assits/WIN_20250219_13_35_27_Pro.jpeg"} width={400} className='mx-auto w-full rounded-2xl' height={400} alt=' ' />
+                            <Image unoptimized src={"https://i.postimg.cc/GtbmZZSy/sallam.png"} width={400} className='mx-auto w-full rounded-2xl' height={400} alt=' ' />
+                            <div className='morethan450 rounded-2xl flex justify-center items-end absolute top-0 left-0 right-0 bottom-0 '>
+                                <div className='flex w-[70%] justify-around items-center h-2/5'>
+                                    <div className='w-1/3'>
+                                        <div className='w-[50px] h-[50px] bg-[#258a20] border border-[#14ff72] rounded-xl'> </div>
+                                    </div>
+                                    <div className='w-2/3 text-center flex justify-center flex-col'>
+                                        <h4 className='text-center font-vazir text-white  font-extrabold text-base'>عبدالله سلام</h4>
+                                        <p className='text-center font-vazir text-gray-400  font-thin text-xs'>Founder of CH Beauty</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='md:w-1/3 w-full'>
+                    <div className='sm:w-[70%] my-5  md:w-[80%] lg:w-[70%]  mx-auto overflow-hidden'>
+                        <div className='relative rounded-2xl overflow-hidden bg-slate-400'>
+                            <Image unoptimized src={"https://i.postimg.cc/PJK5h2BF/nagaty.png"} width={400} className='mx-auto w-full rounded-2xl' height={400} alt=' ' />
                             <div className='morethan450 rounded-2xl flex justify-center items-end absolute top-0 left-0 right-0 bottom-0 '>
                                 <div className='flex w-[70%] justify-around items-center h-2/5'>
                                     <div className='w-1/3'>
@@ -36,33 +55,14 @@ export default function MoreThan450() {
                 <div className='md:w-1/3 w-full'>
                     <div className='sm:w-[70%] my-5  md:w-[80%] lg:w-[70%]  mx-auto overflow-hidden'>
                         <div className='relative rounded-2xl overflow-hidden bg-slate-400'>
-                            <Image unoptimized src={"/assits/WIN_20250219_13_35_27_Pro.jpeg"} width={400} className='mx-auto w-full rounded-2xl' height={400} alt=' ' />
+                            <Image unoptimized src={"https://i.postimg.cc/ZYLRr5CF/khalel.png"} width={400} className='mx-auto w-full rounded-2xl' height={400} alt=' ' />
                             <div className='morethan450 rounded-2xl flex justify-center items-end absolute top-0 left-0 right-0 bottom-0 '>
                                 <div className='flex w-[70%] justify-around items-center h-2/5'>
                                     <div className='w-1/3'>
                                         <div className='w-[50px] h-[50px] bg-[#258a20] border border-[#14ff72] rounded-xl'> </div>
                                     </div>
                                     <div className='w-2/3 text-center flex justify-center flex-col'>
-                                        <h4 className='text-center font-vazir text-white  font-extrabold text-base'>محمد ابو النجا نجاتي</h4>
-                                        <p className='text-center font-vazir text-gray-400  font-thin text-xs'>Founder of CH Beauty</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='md:w-1/3 w-full'>
-                    <div className='sm:w-[70%] my-5  md:w-[80%] lg:w-[70%]  mx-auto overflow-hidden'>
-                        <div className='relative rounded-2xl overflow-hidden bg-slate-400'>
-                            <Image unoptimized src={"/assits/WIN_20250219_13_35_27_Pro.jpeg"} width={400} className='mx-auto w-full rounded-2xl' height={400} alt=' ' />
-                            <div className='morethan450 rounded-2xl flex justify-center items-end absolute top-0 left-0 right-0 bottom-0 '>
-                                <div className='flex w-[70%] justify-around items-center h-2/5'>
-                                    <div className='w-1/3'>
-                                        <div className='w-[50px] h-[50px] bg-[#258a20] border border-[#14ff72] rounded-xl'> </div>
-                                    </div>
-                                    <div className='w-2/3 text-center flex justify-center flex-col'>
-                                        <h4 className='text-center font-vazir text-white  font-extrabold text-base'>محمد ابو النجا نجاتي</h4>
+                                        <h4 className='text-center font-vazir text-white  font-extrabold text-base'>عبدالله سلام</h4>
                                         <p className='text-center font-vazir text-gray-400  font-thin text-xs'>Founder of CH Beauty</p>
                                     </div>
                                 </div>
